1a639b3f8397aa15a07f3427fb31ab9106705e5d
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-08-14 09:25:36 +0900

    Store 무엇이고, 리듀서 함수가 무엇인지 알아보기

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,9 +1,21 @@
+import { createStore } from "redux";
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 let count = 0;
 
+// define reducer
+const countModifier = (state = 0) => {
+  // ... modify state
+
+  return state;
+};
+
+// created store
+const countStore = createStore(countModifier);
+
+console.log(countStore.getState());
 number.innerText = count;
 
 const updateText = () => {
