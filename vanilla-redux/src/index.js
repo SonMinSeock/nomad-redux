ac0095f85f9a462c2c578c87a456f09fd84382e7
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-08-14 09:47:35 +0900

    action, dispatch에 대해 알아보기

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -3,34 +3,45 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
 // define reducer
-const countModifier = (state = 0) => {
+const countModifier = (count = 0, action) => {
   // ... modify state
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "SUB") {
+    return count - 1;
+  }
 
-  return state;
+  return count;
 };
 
 // created store
 const countStore = createStore(countModifier);
+let count = countStore.getState();
 
-console.log(countStore.getState());
-number.innerText = count;
+console.log(`Dispatch 이전 count 결과 : ${count}`);
 
-const updateText = () => {
-  number.innerText = count;
-};
+countStore.dispatch({ type: "ADD" });
 
-const handleAdd = () => {
-  count += 1;
-  updateText();
-};
+count = countStore.getState();
 
-const handleMinus = () => {
-  count -= 1;
-  updateText();
-};
+console.log(`Dispatch 이후 count 결과 : ${count}`);
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+// number.innerText = count;
+
+// const updateText = () => {
+//   number.innerText = count;
+// };
+
+// const handleAdd = () => {
+//   count += 1;
+//   updateText();
+// };
+
+// const handleMinus = () => {
+//   count -= 1;
+//   updateText();
+// };
+
+// add.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
