ae51375a0c132e9249f62e995e1ae294e8b3080d
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-08-30 20:30:59 +0900

    리덕스 원칙 알아본뒤 State Mutation에 대해 이해보기

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -8,10 +8,9 @@ const ADD_TODO = "ADD_TODO";
 const DELET_TODO = "DELETE_TODO";
 
 const reducer = (state = [], action) => {
-  console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      return [...state, { text: action.text, id: Date.now() }];
     case DELET_TODO:
       return [];
     default:
@@ -19,7 +18,13 @@ const reducer = (state = [], action) => {
   }
 };
 
+const onChange = () => {
+  console.log(store.getState());
+};
+
 const store = createStore(reducer);
+
+store.subscribe(onChange);
 
 const onSubmit = (event) => {
   event.preventDefault();
