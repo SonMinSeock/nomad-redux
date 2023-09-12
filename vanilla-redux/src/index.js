96d994a62e2ff6842ab65aece87631c12b03ed21
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-09-12 18:45:20 +0900

    일정 추가, 삭제를 위한 사용자 정의 dispatch 함수 만들기

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -10,7 +10,7 @@ const DELET_TODO = "DELETE_TODO";
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];
+      return [{ text: action.text, id: Date.now() }, ...state];
     case DELET_TODO:
       return [];
     default:
@@ -18,19 +18,50 @@ const reducer = (state = [], action) => {
   }
 };
 
-const onChange = () => {
-  console.log(store.getState());
+const addToDo = (text) => {
+  return { type: ADD_TODO, text };
+};
+
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const deleteToDo = (id) => {
+  return { type: DELET_TODO, id };
+};
+const dispatchDeleteToDo = (event) => {
+  const deleteListId = event.target.parentNode.id;
+
+  store.dispatch(deleteToDo(deleteListId));
+};
+
+const paintToDo = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const deleteBtn = document.createElement("button");
+
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    deleteBtn.innerText = "Delete";
+
+    li.appendChild(deleteBtn);
+    ul.appendChild(li);
+
+    deleteBtn.addEventListener("click", dispatchDeleteToDo);
+  });
 };
 
 const store = createStore(reducer);
 
-store.subscribe(onChange);
+store.subscribe(paintToDo);
 
 const onSubmit = (event) => {
   event.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
