6cd8e7c182d2ed2233f53ab3394b234ea5e9598c
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-10-07 22:15:30 +0900

    To Do 추가, useDispatch, useSelector 훅 이용하기

--- vanilla-redux/src/routes/Home.jsx
+++ vanilla-redux/src/routes/Home.jsx
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { actionCreators } from "../redux/store";
+import ToDo from "../components/ToDo";
 
-function Home({ toDos, addToDo }) {
+function Home() {
   const [text, setText] = useState("");
+  const toDos = useSelector((state) => state);
+  const dispatch = useDispatch();
+
   const onChange = (event) => {
     setText(event.target.value);
   };
 
   const onSubmit = (event) => {
     event.preventDefault();
-    addToDo(text);
+
+    dispatch(actionCreators.addToDoAction(text));
     setText("");
   };
+
   return (
     <>
       <h1>To Do</h1>
@@ -20,17 +26,13 @@ function Home({ toDos, addToDo }) {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul>{JSON.stringify(toDos)}</ul>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
     </>
   );
 }
 
-function mapStateToProps(state) {
-  return { toDos: state };
-}
-
-function mapDispatchToProps(dispatch) {
-  return { addToDo: (text) => dispatch(actionCreators.addToDoAction(text)) };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
