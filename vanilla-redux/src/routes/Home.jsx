18f123fd39789eb82494a0e0bcc3468f62372930
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2024-10-07 17:53:02 +0900

    Setup

---
+++ vanilla-redux/src/routes/Home.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function Home() {
+  const [text, setText] = useState("");
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setText("");
+  };
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul></ul>
+    </>
+  );
+}
+
+export default Home;
